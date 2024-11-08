2d981a698be5badc24eb0853b835a1f8f962c971
Rodri <r.serranvives@gmail.com>
2024-11-08 23:42:42 +0530

    not pushed directly to main

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.use(express.static("dist"));
 app.get('/version', (req, res) => {
   res.send('new feature skipped') // change this string to ensure a new version deployed
 })
+app.get('/test', (req, res) => {
+  res.send('this should not be allowed to be pushed directly to main') // change this string to ensure a new version deployed
+})
 
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
