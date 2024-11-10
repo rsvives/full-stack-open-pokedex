21c5f63dd23dc22ae539a6da8d58ec0362caba8d
Rodri <r.serranvives@gmail.com>
2024-11-10 11:59:37 +0530

    health check broken endpoint

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.get('/test', (req, res) => {
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
   if (true) throw ('error...  ')
+
   res.send('ok')
 })
 
